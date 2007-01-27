7980e3b17212bb37c1a01fd315c75db0b5fa80e7
bdpayne <bdpayne@2fb1d54f-f24a-0410-a3c9-4d2000aea2dc>
2007-01-27 14:02:00 +0000

    code for new example

---
+++ libxa/examples/map-addr.c
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2005  Bryan D. Payne (bryan@thepaynes.cc)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ * --------------------
+ * This file provides a simple example for viewing a kernel address in memory.
+ *
+ * File: map-addr.c
+ *
+ * Author(s): Bryan D. Payne (bryan@thepaynes.cc)
+ *
+ * $Id$
+ * $Date$
+ */
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <stdio.h>
+#include <xenaccess/xenaccess.h>
+#include <xenaccess/xa_private.h>
+
+int main (int argc, char **argv)
+{
+    xa_instance_t xai;
+    unsigned char *memory = NULL;
+    uint32_t offset;
+
+    /* this is the domain ID that we are looking at */
+    uint32_t dom = atoi(argv[1]);
+
+    /* this is the address to map */
+    char *addr_str = argv[2];
+    uint32_t addr = (uint32_t) strtoul(addr_str, NULL, 16);
+
+    /* initialize the xen access library */
+    if (xa_init(dom, &xai) == XA_FAILURE){
+        perror("failed to init XenAccess library");
+        goto error_exit;
+    }
+
+    /* get the symbol's memory page */
+    memory = xa_access_virtual_address(&xai, addr, &offset);
+    if (NULL == memory){
+        perror("failed to get symbol's memory");
+        goto error_exit;
+    }
+    printf("offset = 0x%.8x\n", offset);
+    print_hex(memory, XA_PAGE_SIZE);
+
+error_exit:
+
+    /* cleanup any memory associated with the XenAccess instance */
+    xa_destroy(&xai);
+
+    /* sanity check to unmap shared pages */
+    if (memory) munmap(memory, XA_PAGE_SIZE);
+
+    return 0;
+}
+
