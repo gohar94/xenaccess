d34f6ccaa207940b678fa673c0e8439a1e978623
bdpayne <bdpayne@2fb1d54f-f24a-0410-a3c9-4d2000aea2dc>
2009-05-29 14:59:57 +0000

    added check for new xen version 3.4.0

--- libxa/xenaccess/xa_core.c
+++ libxa/xenaccess/xa_core.c
@@ -404,6 +404,10 @@ void init_xen_version (xa_instance_t *instance)
         instance->m.xen.xen_version = XA_XENVER_3_3_1;
         xa_dbprint("**set instance->m.xen.xen_version = 3.3.1\n");
     }
+    else if (fnmatch("3.4.0*", versionStr, 0) == 0){
+        instance->m.xen.xen_version = XA_XENVER_3_4_0;
+        xa_dbprint("**set instance->m.xen.xen_version = 3.4.0\n");
+    }
 
     if (instance->m.xen.xen_version == XA_XENVER_UNKNOWN){
         fprintf(stderr, "WARNING: This Xen version not supported by XenAccess ");
